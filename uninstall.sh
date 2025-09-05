15e60969c2bbc1066474537e5f2cd747b059899d
Danang Tri Atmaja <114817148+andknownmaly@users.noreply.github.com>
2025-09-05 20:33:22 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -11,13 +11,6 @@ fi
 
 echo "Starts the OpenGUI removal process..."
 
-if [[ -f /usr/share/applications/opengui.desktop ]]; then
-    echo "Delete desktop entries..."
-    sudo rm -f /usr/share/applications/opengui.desktop
-else
-    echo "Desktop entry not found, skip this step."
-fi
-
 if [[ -d /opt/opengui ]]; then
     echo "Deleting the /opt/opengui folder..."
     sudo rm -rf /opt/opengui
@@ -25,11 +18,25 @@ else
     echo "Folder /opt/opengui not found, skip this step."
 fi
 
-if [[ -f /usr/bin/opengui ]]; then
+if [[ -f /usr/local/bin/opengui ]]; then
     echo "Removing opengui binaries..."
-    sudo rm -f /usr/bin/opengui
+    sudo rm -f /usr/local/bin/opengui
 else
     echo "Opengui binary not found, skip this step."
+fi
+
+if [[ -f /usr/share/applications/opengui.desktop ]]; then
+    echo "Delete desktop entries..."
+    sudo rm -f /usr/share/applications/opengui.desktop
+else
+    echo "Desktop entry not found, skip this step."
+fi
+
+read -p "Remove user configurations? (y/N) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm -rf ~/.config/openvpn-gui
 fi
 
 echo "The OpenGUI removal process is complete."
